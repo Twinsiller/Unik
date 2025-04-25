086f02ad544d634b8b5201efe8ea887d3a197d73
Twinsiller <boldinov.leha@yandex.ru>
2025-04-26 02:45:43 +0300

    o

--- S2/C sharp/Practices/Pr1/obj/Debug/net8.0/Pr1.AssemblyInfo.cs
+++ S2/C sharp/Practices/Pr1/obj/Debug/net8.0/Pr1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Pr1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+66f9ea685ad7d8a52b399f13bbc917425dc03441")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+80ba6cfb6a6092c15a752267e7fa0e6f670abfc6")]
 [assembly: System.Reflection.AssemblyProductAttribute("Pr1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Pr1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- S2/C sharp/Practices/Pr3-Server/obj/Debug/net8.0-windows/App.g.i.cs
+++ S2/C sharp/Practices/Pr3-Server/obj/Debug/net8.0-windows/App.g.i.cs
@@ -59,7 +59,7 @@ namespace Pr3_Server {
             
             #line default
             #line hidden
-            System.Uri resourceLocater = new System.Uri("/Pr3-Server;component/app.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/Pr3-Server;V1.0.0.0;component/app.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\App.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
